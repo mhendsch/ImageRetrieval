717381d720f279b973638f4b30e33190878fc921
mhendsch <mhendsch@bu.edu>
2026-04-23 13:25:13 -0400

    Implement submit_image

--- src/cli_service.py
+++ src/cli_service.py
@@ -2,13 +2,28 @@ import redis
 import json
 import uuid
 from dotenv import load_dotenv
+from datetime import datetime, timezone
 import os
-from redis_client import r
+from redis_client import r, publish, subscribe
 
 # Query and simulated uploads
 
-def submit_image():
-    return
+def submit_image(path: str, source: str):
+    image_id = str(uuid.uuid4()) # Simulated, for now
+
+    publish("image.submitted", {
+        "type": "publish",
+        "topic": "image.submitted",
+        "event_id": str(uuid.uuid4())
+        "payload": {
+            "image_id": image_id,
+            "path": path,
+            "source": source,
+            "timestamp": datetime.now(timezone.utc).isoformat()
+        }
+    })
+    print(f"[cli_service] Submitted image {image_id} from {source}")
+    return image_id
 
 def submit_query():
     return
